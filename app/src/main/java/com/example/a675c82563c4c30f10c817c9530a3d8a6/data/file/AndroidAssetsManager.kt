d8a033cb4bac05cba8183996f7133ff0b2534990
Ömer Yıldız <yildizomer94@gmail.com>
2023-07-23 18:18:44 +0300

    Add json converter gson to AndroidAssetsManager

--- app/src/main/java/com/example/a675c82563c4c30f10c817c9530a3d8a6/data/file/AndroidAssetsManager.kt
+++ app/src/main/java/com/example/a675c82563c4c30f10c817c9530a3d8a6/data/file/AndroidAssetsManager.kt
@@ -1,9 +1,13 @@
 package com.example.a675c82563c4c30f10c817c9530a3d8a6.data.file
 
 import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class AndroidAssetsManager(private val context: Context): AssetsManager {
-    override fun readAssets(path: String): String {
-        return context.assets.open(path).bufferedReader().use { it.readText() }
+    override fun <T> readAssets(path: String): T {
+        val jsonString = context.assets.open(path).bufferedReader().use { it.readText() }
+        val typeToken = object: TypeToken<T>(){}.type
+        return Gson().fromJson(jsonString, typeToken)
     }
 }
