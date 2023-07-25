bf288f4e53e0b8e32111585a7c8941656a0795c8
Ömer Yıldız <yildizomer94@gmail.com>
2023-07-25 21:30:04 +0300

    Add first query control to satellite list

--- app/src/main/java/com/example/a675c82563c4c30f10c817c9530a3d8a6/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/a675c82563c4c30f10c817c9530a3d8a6/ui/list/ListViewModel.kt
@@ -8,7 +8,8 @@ import com.example.a675c82563c4c30f10c817c9530a3d8a6.domain.model.Satellite
 import com.example.a675c82563c4c30f10c817c9530a3d8a6.domain.usecase.GetSatellitesUseCase
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -18,12 +19,13 @@ class ListViewModel @Inject constructor(
     private val getSatellitesUseCase: GetSatellitesUseCase
 ): ViewModel() {
 
-    private val _items = MutableSharedFlow<Res<List<Satellite>>>()
-    val items get() = _items
+    private val _items = MutableStateFlow<Res<List<Satellite>>>(Res.Loading)
+    val items get() = _items.asStateFlow()
     val ofLoading = ObservableField(true)
 
     private val searchDelay: Long = 800L
     private var searchJob: Job? = null
+    private var isFirstQuery = true
 
     init {
         getItems()
@@ -33,11 +35,14 @@ class ListViewModel @Inject constructor(
         searchJob?.cancel()
         searchJob = viewModelScope.launch {
             ofLoading.set(true)
-            delay(searchDelay)
+            if (!isFirstQuery) {
+                delay(searchDelay)
+            }
             getSatellitesUseCase(query).onEach {
                 ofLoading.set(it is Res.Loading)
                 _items.emit(it)
             }.launchIn(this)
+            isFirstQuery = false
         }
     }
 }
